fab58ab5eb53df889d2194788b529ea8c146ea45
Martin Drahony <martin@mdrahony.online>
2019-10-03 07:51:22 +0100

    modifed website data in seeder

--- database/seeds/CompaniesTableSeeder.php
+++ database/seeds/CompaniesTableSeeder.php
@@ -16,25 +16,25 @@ class CompaniesTableSeeder extends Seeder
             'name' => 'First Company',
             'email' => 'queries@firstcompany.com',
             'logo' => '',
-            'website' => 'https://www.firstcompany.co.uk/'
+            'website' => 'www.firstcompany.co.uk'
         ]);
         DB::table('companies')->insert([
             'name' => 'Second Company',
             'email' => 'queries@secondcompany.com',
             'logo' => '',
-            'website' => 'https://www.secondcompany.co.uk/'
+            'website' => 'www.secondcompany.co.uk'
         ]);
         DB::table('companies')->insert([
             'name' => 'Third Company',
             'email' => 'queries@thirdcompany.com',
             'logo' => '',
-            'website' => 'https://www.thirdcompany.co.uk/'
+            'website' => 'www.thirdcompany.co.uk'
         ]);
         DB::table('companies')->insert([
             'name' => 'Fourth Company',
             'email' => 'queries@fourthcompany.com',
             'logo' => '',
-            'website' => 'https://www.fourthcompany.co.uk/'
+            'website' => 'www.fourthcompany.co.uk'
         ]);
     }
 }
