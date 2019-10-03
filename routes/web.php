0498dbf2354dd599d826ef4af1a9c9c103457f5f
Martin Drahony <martin@mdrahony.online>
2019-10-03 08:55:41 +0100

    added employee routes

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,6 @@ Route::post('/companies/delete/{id}', 'CompaniesController@destroy')->name('comp
 
 //employees
 Route::get('/employees', 'EmployeesController@index')->name('employees');
+
+Route::get('/employees/create', 'EmployeesController@create')->name('employees.create');
+Route::post('/employees/store', 'EmployeesController@store')->name('employees.store');
