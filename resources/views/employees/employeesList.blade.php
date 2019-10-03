a5e29c00695be39b7ff22f4b48e0aed2bdfaea52
Martin Drahony <martin@mdrahony.online>
2019-10-03 16:28:19 +0100

    employee show link added

--- resources/views/employees/employeesList.blade.php
+++ resources/views/employees/employeesList.blade.php
@@ -4,20 +4,22 @@
 <div class="col-12 col-sm-10 col-md-10 col-lg-6 col-xl-6 offset-0 offset-sm-1 offset-md-1 offset-lg-3 offset-xl-3 p-0">
 
     @foreach ($employees as $employee)
-    <div class="d-flex flex-wrap p-3 mb-3 listItem" style="">
-        <div
-            style="overflow:hidden;margin-right:20px;width:100px;height:100px;border:2px solid #555;border-radius:8px;">
-            <img src="{{ asset('storage') . '/'.$employee->profileImage()  }}" alt="" class="" style="width:100px;">
+    <a href="{{ route('employees.show',$employee->id)}}">
+        <div class="d-flex flex-wrap p-3 mb-3 listItem" style="">
+            <div
+                style="overflow:hidden;margin-right:20px;width:100px;height:100px;border:2px solid #555;border-radius:8px;">
+                <img src="{{ asset('storage') . '/'.$employee->profileImage()  }}" alt="" class="" style="width:100px;">
+            </div>
+            <div>
+               
+                <h3><strong>{{ $employee->first_name}} {{ $employee->last_name}}</strong></h3>
+                <h5><i class="fa fa-building"></i>
+                    <a href="{{ route('companies.show',$employee->company->id)}}"> {{ $employee->company->name}}</a>
+                </h5>
+              
+            </div>
         </div>
-        <div>
-            <h3><strong>{{ $employee->first_name}} {{ $employee->last_name}}</strong></h3>
-            <h5><i class="fa fa-building"></i>
-                <a href="{{ route('companies.show',$employee->company->id)}}"> {{ $employee->company->name}}</a>
-            </h5>
-            <h5><i class="fa fa-envelope"></i><a href="mailto:{{ $employee->email}}"> {{ $employee->email}}</a></h5>
-            <h5><i class="fa fa-phone"></i> {{ $employee->phone}}</h5>
-        </div>
-    </div>
+    </a>
     @endforeach
     <div class="row">
         <div class="col-12 d-flex justify-content-center">
