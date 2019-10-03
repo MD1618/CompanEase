03a20ef423b001df0c31056aca69fa3074e08f03
Martin Drahony <martin@mdrahony.online>
2019-10-03 13:13:58 +0100

    moved idon outside a link

--- resources/views/companies/companiesList.blade.php
+++ resources/views/companies/companiesList.blade.php
@@ -5,19 +5,18 @@
 
     @foreach ($companies as $company)
 
-<a href="{{ route('companies.show', $company->id)}}" style="text-decoration: none;color:#333;">
-        <div class="d-flex flex-wrap p-3 mb-3 shadow listItem" style="">
-            <div style=";margin-right:20px;width:100px;height:100px;">
-                <img src="{{ asset('storage') .'/'. $company->logoImage() }}" alt="" style="width:100px;">
+    <a href="{{ route('companies.show', $company->id)}}" style="text-decoration: none;color:#333;">
+        <div class="d-flex flex-wrap p-3 mb-3 listItem" style="">
+            <div style=";margin-right:20px;width:130px;height:130px;">
+                <img src="{{ asset('storage') .'/'. $company->logoImage() }}" alt="" style="width:130px;">
             </div>
             <div>
                 <h3><strong>{{ $company->name}}</strong></h3>
-                <h5><i class="fa fa-envelope"></i> {{ $company->email}}</h5>
-                <h5>{{ $company->website}}</h5>
+                <h5><i class="fa fa-envelope"></i><a href="mailto:{{ $company->email}}"> {{ $company->email}}</a></h5>
+                <h5><a href="http://{{ $company->website}}">{{ $company->website}}</a></h5>
                 <h5>Employees {{ $company->employees->count()}}</h5>
             </div>
-            {{-- <div style="position:absolute;top:10px;right:20px;font-size:20px;"><a href="" style="color:#666;"><i class="fa fa-trash" ></i></a></div> --}}
-
+           
         </div>
     </a>
     @endforeach
