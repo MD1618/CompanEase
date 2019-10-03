d731653e9d7c36205c70b55480c4c2e2575ccf8f
Martin Drahony <martin@mdrahony.online>
2019-10-03 08:50:02 +0100

    bootstrap screen size variations

--- resources/views/companies/companiesList.blade.php
+++ resources/views/companies/companiesList.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="col-4 offset-4">
+<div class="col-12 col-sm-10 col-md-10 col-lg-6 col-xl-6 offset-0 offset-sm-1 offset-md-1 offset-lg-3 offset-xl-3 p-0">
 
     @foreach ($companies as $company)
 
-    <div class="d-flex p-3 mb-3 shadow listItem" style="background:#fff;border-radius:7px;">
+    <div class="d-flex flex-wrap p-3 mb-3 shadow listItem" style="background:#fff;border-radius:7px;">
         <div
             style=";margin-right:20px;width:100px;height:100px;">
             <img src="{{ asset('storage') .'/'. $company->logoImage() }}" alt="" style="width:100px;">
--- resources/views/employees/employeesList.blade.php
+++ resources/views/employees/employeesList.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="col-4 offset-4">
+<div class="col-12 col-sm-10 col-md-10 col-lg-6 col-xl-6 offset-0 offset-sm-1 offset-md-1 offset-lg-3 offset-xl-3 p-0">
 
     @foreach ($employees as $employee)
-    <div class="d-flex p-3 mb-3  shadow listItem" style="background:#fff;border-radius:7px;">
+    <div class="d-flex flex-wrap p-3 mb-3 shadow listItem" style="background:#fff;border-radius:7px;">
         <div
             style="overflow:hidden;margin-right:20px;width:100px;height:100px;border:2px solid #555;border-radius:8px;">
             <img src="{{ asset('storage') . '/'.$employee->profileImage()  }}" alt="" class="" style="width:100px;">
