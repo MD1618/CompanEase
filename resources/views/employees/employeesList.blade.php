95d4cb4b3d0731437ee999b2efd48ccfe331bd1b
Martin Drahony <martin@mdrahony.online>
2019-10-03 11:41:50 +0100

    icons added and style changes

--- resources/views/employees/employeesList.blade.php
+++ resources/views/employees/employeesList.blade.php
@@ -4,16 +4,16 @@
 <div class="col-12 col-sm-10 col-md-10 col-lg-6 col-xl-6 offset-0 offset-sm-1 offset-md-1 offset-lg-3 offset-xl-3 p-0">
 
     @foreach ($employees as $employee)
-    <div class="d-flex flex-wrap p-3 mb-3 shadow listItem" style="background:#fff;border-radius:7px;">
+    <div class="d-flex flex-wrap p-3 mb-3 shadow listItem" style="">
         <div
             style="overflow:hidden;margin-right:20px;width:100px;height:100px;border:2px solid #555;border-radius:8px;">
             <img src="{{ asset('storage') . '/'.$employee->profileImage()  }}" alt="" class="" style="width:100px;">
         </div>
         <div>
-            <h3>{{ $employee->first_name}} {{ $employee->last_name}}</h3>
-            <h5>{{ $employee->email}}</h5>
-            <h5>{{ $employee->phone}}</h5>
-            <h5>Works At {{ $employee->company->name}}</h5>
+            <h3><strong>{{ $employee->first_name}} {{ $employee->last_name}}</strong></h3>
+            <h5><i class="fa fa-building"></i>  {{ $employee->company->name}}</h5>
+            <h5><i class="fa fa-envelope"></i>  {{ $employee->email}}</h5>
+            <h5><i class="fa fa-phone"></i>  {{ $employee->phone}}</h5>
         </div>
     </div>
     @endforeach
