9a11615e4e9783b5286a21c5b791ab7b24532914
Martin Drahony <martin@mdrahony.online>
2019-10-04 15:52:57 +0100

    add qualifications form blade created

---
+++ resources/views/qualifications/qualificationCreate.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+
+@section('content')
+<form action="{{ route('employees.store') }}" enctype="multipart/form-data" method="POST">
+    @csrf
+    <div class="row">
+        <div
+            class="col-10 col-sm-10 col-md-10 col-lg-6 col-xl-6 offset-1 offset-sm-1 offset-md-1 offset-lg-3 offset-xl-3">
+
+            <div class="row">
+                <h2>Add New Qualification</h2>
+            </div>
+
+            <div class="form-group row">
+                <label for="title" class="col-md-4 col-form-label ">{{ __('Title') }}</label>
+
+                <input id="title" name="title" type="text"
+                    class="form-control @error('title') is-invalid @enderror" value="{{ old('title') }}"
+                    required autocomplete="title" autofocus>
+
+                @error('title')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            </div>
+
+         
+
+            <div class="form-group row">
+                <label for="degree" class="col-md-4 col-form-label ">{{ __('Degree Level') }}</label>
+
+                <select class="form-control" id="degree" name="degree" required>
+                    <option value="" selected disabled>Level</option>
+                    <option value="HNC">HNC</option>
+                    <option value="HND">HND</option>
+                    <option value="B.S">B.S</option>
+                    <option value="Ph.D">Ph.D</option>
+                    <option value="M.S">M.S</option>
+                    
+
+                </select>
+            </div>
+
+                   
+
+            <div class="row pt-4">
+                <button class="btn btn-primary">Add Qualification</button>
+            </div>
+
+        </div>
+    </div>
+
+
+</form>
+@endsection
