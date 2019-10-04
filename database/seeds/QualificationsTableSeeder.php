74dd6fcd05171397a3dc540cb6a1fc8c131b8904
Martin Drahony <martin@mdrahony.online>
2019-10-04 16:03:10 +0100

    filled out the qualifications seeder

--- database/seeds/QualificationsTableSeeder.php
+++ database/seeds/QualificationsTableSeeder.php
@@ -13,19 +13,56 @@ class QualificationsTableSeeder extends Seeder
     {
         //
         DB::table('qualifications')->insert([
-            'title' => 'HND Software Development'
-        ]);
-        DB::table('qualifications')->insert([
-            'title' => 'PH.D Software Development'
-        ]);
-        DB::table('qualifications')->insert([
-            'title' => 'B.S Software Development'
+            'title' => 'No Qualifications'
         ]);
         DB::table('qualifications')->insert([
             'title' => 'HNC Software Development'
         ]);
         DB::table('qualifications')->insert([
+            'title' => 'HND Software Development'
+        ]);
+        DB::table('qualifications')->insert([
+            'title' => 'B.S Software Development'
+        ]);
+        DB::table('qualifications')->insert([
+            'title' => 'PH.D Software Development'
+        ]);
+        DB::table('qualifications')->insert([
             'title' => 'M.S Software Development'
+        ]);
+
+        DB::table('qualifications')->insert([
+            'title' => 'HNC Web Development'
+        ]);
+        DB::table('qualifications')->insert([
+            'title' => 'HND Web Development'
+        ]);
+        
+        DB::table('qualifications')->insert([
+            'title' => 'B.S Web Development'
+        ]);
+        DB::table('qualifications')->insert([
+            'title' => 'PH.D Web Development'
+        ]);
+        DB::table('qualifications')->insert([
+            'title' => 'M.S Web Development'
+        ]);
+
+        DB::table('qualifications')->insert([
+            'title' => 'HNC Computer Science'
+        ]);
+        DB::table('qualifications')->insert([
+            'title' => 'HND Computer Science'
+        ]);
+       
+        DB::table('qualifications')->insert([
+            'title' => 'B.S Computer Science'
+        ]);
+        DB::table('qualifications')->insert([
+            'title' => 'PH.D Computer Science'
+        ]);
+        DB::table('qualifications')->insert([
+            'title' => 'M.S Computer Science'
         ]);
     }
 }
