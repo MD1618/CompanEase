c767cbbd7c0b7766dacd6acad1746a46b95c260a
Martin Drahony <martin@mdrahony.online>
2019-10-04 15:55:46 +0100

    styleing changes

--- resources/views/employees/employeesList.blade.php
+++ resources/views/employees/employeesList.blade.php
@@ -6,13 +6,12 @@
     @foreach ($employees as $employee)
     <a href="{{ route('employees.show',$employee->id)}}">
         <div class="d-flex flex-wrap p-3 mb-3 listItem" style="">
-            <div
-                style="overflow:hidden;margin-right:20px;width:100px;height:100px;border:2px solid #555;border-radius:8px;">
+            <div style="overflow:hidden;margin:auto 20px;width:100px;height:100px;border:2px solid #555;border-radius:8px;">
                 <img src="{{ asset('storage') . '/'.$employee->profileImage()  }}" alt="" class="" style="width:100px;">
             </div>
-            <div>
+            <div class="p-3"> 
                
-                <h3><strong>{{ $employee->first_name}} {{ $employee->last_name}}</strong></h3>
+                <h3 style="color:#444;"><strong>{{ $employee->first_name}} {{ $employee->last_name}}</strong></h3>
                 <h5><i class="fa fa-building"></i>
                     <a href="{{ route('companies.show',$employee->company->id)}}"> {{ $employee->company->name}}</a>
                 </h5>
