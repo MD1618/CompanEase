f94e8136d0174632fc6843bb1e99ebbede21929a
Martin Drahony <martin@mdrahony.online>
2019-10-05 15:34:33 +0100

    logic for all the pie chart data added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Company;
+use App\Employee;
+use App\Qualification;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -23,6 +27,41 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+
+        $companiesC = Company::all();
+        $employeeCount = [];
+        foreach ($companiesC as $company) {
+           
+            array_push($employeeCount, (object) ['count'=> $company->employees->count() ]);
+           
+        }
+
+        //dd($employeeCount);
+        $companies = DB::table('companies')->select('name')->get()->toArray();
+        //dd($companies);
+
+        $employeesC = Employee::all();
+        $qualificationCount = [];
+        foreach ($employeesC as $employee) {
+           
+            array_push($qualificationCount, (object) ['count'=> $employee->qualified->count() ]);
+           
+        }
+       //dd($qualificationCount);
+
+        $employees = DB::table('employees')->select('first_name', 'last_name')->get();
+
+
+        $qualificationsC = Qualification::all();
+        $employeeCount = [];
+        foreach ($qualificationsC as $qualification) {
+           
+            array_push($employeeCount, (object) ['count'=> $qualification->student->count() ]);
+           
+        }
+        //dd($employeeCount);
+        $qualifications =  DB::table('qualifications')->select('title')->get();
+
+        return view('home',compact('companies','employeeCount','employees','qualificationCount','qualifications','employeeCount'));
     }
 }
