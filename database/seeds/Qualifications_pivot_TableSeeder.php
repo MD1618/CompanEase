9b973ec2cef591fbb207e1cc84176833b6102de3
Martin Drahony <martin@mdrahony.online>
2019-10-05 15:34:03 +0100

    added more entries

--- database/seeds/Qualifications_pivot_TableSeeder.php
+++ database/seeds/Qualifications_pivot_TableSeeder.php
@@ -24,117 +24,234 @@ class Qualifications_pivot_TableSeeder extends Seeder
             'aquired' => '2019-06-06',
             'grade' => 'A'
         ]);
+
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '3',
+            'employee_id' => '2',
+            'aquired' => '2019-06-06',
+            'grade' => 'A'
+        ]);
+
         DB::table('employee_qualification')->insert([
             'qualification_id' => '6',
-            'employee_id' => '1',
+            'employee_id' => '3',
             'aquired' => '2019-06-06',
             'grade' => 'A'
         ]);
         DB::table('employee_qualification')->insert([
+            'qualification_id' => '7',
+            'employee_id' => '3',
+            'aquired' => '2019-06-06',
+            'grade' => 'A'
+        ]);
+
+
+        DB::table('employee_qualification')->insert([
             'qualification_id' => '4',
-            'employee_id' => '2',
+            'employee_id' => '4',
             'aquired' => '2018-06-06',
             'grade' => 'A'
         ]);
         DB::table('employee_qualification')->insert([
             'qualification_id' => '5',
-            'employee_id' => '2',
+            'employee_id' => '4',
+            'aquired' => '2018-06-06',
+            'grade' => 'A'
+        ]);
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '9',
+            'employee_id' => '4',
+            'aquired' => '2018-06-06',
+            'grade' => 'A'
+        ]);
+
+
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '10',
+            'employee_id' => '5',
             'aquired' => '2017-06-06',
             'grade' => 'A'
         ]);
         DB::table('employee_qualification')->insert([
+            'qualification_id' => '11',
+            'employee_id' => '5',
+            'aquired' => '2017-06-06',
+            'grade' => 'A'
+        ]);
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '12',
+            'employee_id' => '5',
+            'aquired' => '2017-06-06',
+            'grade' => 'A'
+        ]);
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '13',
+            'employee_id' => '5',
+            'aquired' => '2017-06-06',
+            'grade' => 'A'
+        ]);
+
+
+        DB::table('employee_qualification')->insert([
             'qualification_id' => '8',
-            'employee_id' => '3',
+            'employee_id' => '6',
             'aquired' => '2017-06-06',
             'grade' => 'A'
         ]);
         DB::table('employee_qualification')->insert([
             'qualification_id' => '7',
-            'employee_id' => '3',
+            'employee_id' => '7',
             'aquired' => '2016-06-06',
             'grade' => 'A'
         ]);
         DB::table('employee_qualification')->insert([
             'qualification_id' => '7',
-            'employee_id' => '4',
+            'employee_id' => '8',
             'aquired' => '2014-06-06',
             'grade' => 'A'
         ]);
         DB::table('employee_qualification')->insert([
             'qualification_id' => '8',
-            'employee_id' => '4',
+            'employee_id' => '9',
             'aquired' => '2013-06-06',
             'grade' => 'A'
         ]);
         DB::table('employee_qualification')->insert([
             'qualification_id' => '9',
-            'employee_id' => '5',
+            'employee_id' => '10',
             'aquired' => '2012-06-06',
             'grade' => 'A'
         ]);
         DB::table('employee_qualification')->insert([
             'qualification_id' => '8',
-            'employee_id' => '5',
+            'employee_id' => '11',
             'aquired' => '2019-06-06',
             'grade' => 'A'
         ]);
         DB::table('employee_qualification')->insert([
             'qualification_id' => '10',
-            'employee_id' => '6',
+            'employee_id' => '12',
             'aquired' => '2012-06-06',
             'grade' => 'A'
         ]);
         DB::table('employee_qualification')->insert([
             'qualification_id' => '14',
-            'employee_id' => '6',
+            'employee_id' => '13',
             'aquired' => '2011-06-06',
             'grade' => 'A'
         ]);
         DB::table('employee_qualification')->insert([
             'qualification_id' => '13',
-            'employee_id' => '7',
+            'employee_id' => '14',
             'aquired' => '2011-06-06',
             'grade' => 'A'
         ]);
         DB::table('employee_qualification')->insert([
             'qualification_id' => '12',
-            'employee_id' => '7',
+            'employee_id' => '15',
             'aquired' => '2016-06-06',
             'grade' => 'A'
         ]);
         DB::table('employee_qualification')->insert([
             'qualification_id' => '13',
-            'employee_id' => '8',
+            'employee_id' => '16',
             'aquired' => '2011-06-06',
             'grade' => 'A'
         ]);
         DB::table('employee_qualification')->insert([
             'qualification_id' => '3',
-            'employee_id' => '8',
+            'employee_id' => '17',
             'aquired' => '2012-06-06',
             'grade' => 'A'
         ]);
         DB::table('employee_qualification')->insert([
             'qualification_id' => '6',
-            'employee_id' => '4',
+            'employee_id' => '18',
             'aquired' => '2011-06-06',
             'grade' => 'A'
         ]);
         DB::table('employee_qualification')->insert([
             'qualification_id' => '2',
-            'employee_id' => '4',
+            'employee_id' => '19',
             'aquired' => '2011-06-06',
             'grade' => 'A'
         ]);
+
         DB::table('employee_qualification')->insert([
             'qualification_id' => '3',
-            'employee_id' => '4',
+            'employee_id' => '20',
+            'aquired' => '2019-06-06',
+            'grade' => 'A'
+        ]);
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '4',
+            'employee_id' => '20',
+            'aquired' => '2019-06-06',
+            'grade' => 'A'
+        ]);
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '5',
+            'employee_id' => '20',
             'aquired' => '2019-06-06',
             'grade' => 'A'
         ]);
         DB::table('employee_qualification')->insert([
             'qualification_id' => '6',
-            'employee_id' => '3',
+            'employee_id' => '21',
+            'aquired' => '2019-06-06',
+            'grade' => 'A'
+        ]);
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '6',
+            'employee_id' => '22',
+            'aquired' => '2019-06-06',
+            'grade' => 'A'
+        ]);
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '6',
+            'employee_id' => '23',
+            'aquired' => '2019-06-06',
+            'grade' => 'A'
+        ]);
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '6',
+            'employee_id' => '24',
+            'aquired' => '2019-06-06',
+            'grade' => 'A'
+        ]);
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '6',
+            'employee_id' => '25',
+            'aquired' => '2019-06-06',
+            'grade' => 'A'
+        ]);
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '6',
+            'employee_id' => '26',
+            'aquired' => '2019-06-06',
+            'grade' => 'A'
+        ]);
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '6',
+            'employee_id' => '27',
+            'aquired' => '2019-06-06',
+            'grade' => 'A'
+        ]);
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '6',
+            'employee_id' => '28',
+            'aquired' => '2019-06-06',
+            'grade' => 'A'
+        ]);
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '6',
+            'employee_id' => '29',
+            'aquired' => '2019-06-06',
+            'grade' => 'A'
+        ]);
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '6',
+            'employee_id' => '30',
             'aquired' => '2019-06-06',
             'grade' => 'A'
         ]);
