f88ad1c5a8e8234664c7f856c92c4d30768c242c
Martin Drahony <martin@mdrahony.online>
2019-10-05 07:50:57 +0100

    filled the seeder table with more qualifications

--- database/seeds/Qualifications_pivot_TableSeeder.php
+++ database/seeds/Qualifications_pivot_TableSeeder.php
@@ -30,5 +30,113 @@ class Qualifications_pivot_TableSeeder extends Seeder
             'aquired' => '2019-06-06',
             'grade' => 'A'
         ]);
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '4',
+            'employee_id' => '2',
+            'aquired' => '2018-06-06',
+            'grade' => 'A'
+        ]);
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '5',
+            'employee_id' => '2',
+            'aquired' => '2017-06-06',
+            'grade' => 'A'
+        ]);
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '8',
+            'employee_id' => '3',
+            'aquired' => '2017-06-06',
+            'grade' => 'A'
+        ]);
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '7',
+            'employee_id' => '3',
+            'aquired' => '2016-06-06',
+            'grade' => 'A'
+        ]);
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '7',
+            'employee_id' => '4',
+            'aquired' => '2014-06-06',
+            'grade' => 'A'
+        ]);
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '8',
+            'employee_id' => '4',
+            'aquired' => '2013-06-06',
+            'grade' => 'A'
+        ]);
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '9',
+            'employee_id' => '5',
+            'aquired' => '2012-06-06',
+            'grade' => 'A'
+        ]);
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '8',
+            'employee_id' => '5',
+            'aquired' => '2019-06-06',
+            'grade' => 'A'
+        ]);
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '10',
+            'employee_id' => '6',
+            'aquired' => '2012-06-06',
+            'grade' => 'A'
+        ]);
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '14',
+            'employee_id' => '6',
+            'aquired' => '2011-06-06',
+            'grade' => 'A'
+        ]);
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '13',
+            'employee_id' => '7',
+            'aquired' => '2011-06-06',
+            'grade' => 'A'
+        ]);
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '12',
+            'employee_id' => '7',
+            'aquired' => '2016-06-06',
+            'grade' => 'A'
+        ]);
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '13',
+            'employee_id' => '8',
+            'aquired' => '2011-06-06',
+            'grade' => 'A'
+        ]);
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '3',
+            'employee_id' => '8',
+            'aquired' => '2012-06-06',
+            'grade' => 'A'
+        ]);
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '6',
+            'employee_id' => '4',
+            'aquired' => '2011-06-06',
+            'grade' => 'A'
+        ]);
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '2',
+            'employee_id' => '4',
+            'aquired' => '2011-06-06',
+            'grade' => 'A'
+        ]);
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '3',
+            'employee_id' => '4',
+            'aquired' => '2019-06-06',
+            'grade' => 'A'
+        ]);
+        DB::table('employee_qualification')->insert([
+            'qualification_id' => '6',
+            'employee_id' => '3',
+            'aquired' => '2019-06-06',
+            'grade' => 'A'
+        ]);
     }
 }
